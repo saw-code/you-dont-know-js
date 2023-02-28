8349f0ae50ceec5af7d835208b4d2fe97941f3e0
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-02-28 11:33:26 +0300

    input practice

---
+++ up_and_go/chapter-1.js
@@ -0,0 +1,6 @@
+// input
+
+// сообщение "Please tell me your age:" выводится во всплывающем окне
+// где мы можем ввести значение к-е запишется в переменную age
+age = prompt( "Please tell me your age:" );
+console.log( age );
