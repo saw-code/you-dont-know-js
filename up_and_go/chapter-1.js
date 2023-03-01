6abb3d117d867e9902a2f1322f7390fe113ca483
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-03-01 18:49:57 +0300

    types

--- up_and_go/chapter-1.js
+++ up_and_go/chapter-1.js
@@ -2,5 +2,21 @@
 
 // сообщение "Please tell me your age:" выводится во всплывающем окне
 // где мы можем ввести значение к-е запишется в переменную age
-age = prompt( "Please tell me your age:" );
-console.log( age );
+// age = prompt( "Please tell me your age:" );
+// console.log( age );
+
+// typeof null //-> вернет "object" это старый баг js
+// __________________________________________________________________
+let obj = {
+  a: "hello world",
+  b: 42
+};
+
+let c = "a";              // a
+
+console.log(c);
+console.log(obj[c]);			// "hello world"
+console.log(obj["b"]);		// 42
+
+// __________________________________________________________________
+
